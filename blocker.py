9852f220fe5506de20cc350dfd8fd96621559aa1
l33th <l33th@protonmail.ch>
2021-12-20 20:35:45 +0200

    Website Blocker Final Build

--- blocker.py
+++ blocker.py
@@ -14,4 +14,16 @@ Websites.place(x = 140, y = 60)
 def Blocker():
 	website_lists = Websites.get(1.0,END)
 	Website = list(website_lists.split(","))
-	
+	with open (host_path, 'r+') as host_file:
+		file_content = host_file.read()
+		for web in Website:
+			if web in file_content:
+				Label(root, text = 'Already Blocked', font = 'arial 12 bold').place(x=200, y=200)
+				pass
+			else:
+				host_file.write(ip_address + " " + web + '\n')
+				Label(root, text = "Blocked", font = 'arial 12 bold').place(x=200, y=200)
+
+block = Button(root, text = 'Block', font = 'arial 12 bold', pady = 5, command = Blocker, width = 6, bg = 'royal blue1', activebackground = 'sky blue')
+block.place(x = 230, y = 150)
+root.mainloop()
