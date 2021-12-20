e4919a8858c569fb5b2ce0cea1029246bada2e3a
l33th <l33th@protonmail.ch>
2021-12-20 20:29:38 +0200

    Blocker function setup

--- blocker.py
+++ blocker.py
@@ -9,3 +9,9 @@ ip_address = '127.0.0.1'
 Label(root, text = 'Enter Website : ', font = 'arial 13 bold').places(x=5, y=60)
 Websites = Text(root, font = 'arial 10', height = '2', width = '40')
 Websites.place(x = 140, y = 60)
+
+
+def Blocker():
+	website_lists = Websites.get(1.0,END)
+	Website = list(website_lists.split(","))
+	
