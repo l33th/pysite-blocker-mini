3995b3f59f19dbab1caaf3c04e2df2e200579dca
l33th <l33th@protonmail.ch>
2021-12-20 20:19:39 +0200

    added windows hosts path

--- blocker.py
+++ blocker.py
@@ -4,3 +4,4 @@ root.geometry('500x300')
 root.resizable(0,0)
 root.title("Website Blocker")
 Label(root, text='Website Blocker', font='arial 20 bold').pack()
+host_path = `C:\Windows\System32\drivers\etc\hosts`
